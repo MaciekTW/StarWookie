86b5d118d15e3ec4807068459d4f3d2da1273fc5
Jofny <woda254@gmail.com>
2023-05-15 10:57:59 +0200

    Changing the logo and removing shadow from it

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,5 +1,5 @@
 <div {{ $attributes->merge(['style' => '', 'class' => 'flex justify-center items-center text-center']) }}>
-    <img class="w-14 h-14 object-contain shadow-xl" src="{{ asset('Graphics/falcon.png') }}"/>
+    <img class="w-14 h-14" src="{{ asset('Graphics/falcon.png') }}"/>
     <div class="hidden lg:block">
         <p class="text-white text-4xl px-2 text-center">Wookiepedia</p>
     </div>
